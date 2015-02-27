468f39e6c3338cec083da4fc2ffd3b534ad2a6cd
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-02-26 17:53:03 -0800

    updated pbd-mpi.R

--- pbd-mpi.R
+++ pbd-mpi.R
@@ -7,9 +7,9 @@ init()
 myRank <- comm.rank () # comm index starts at 0 , not 1
 comm.print(myRank , all.rank=TRUE)
 node <- Sys.getenv("HOSTNAME")
-if(myRank == 0) 
+if(myRank == 0) {
     comm.print(paste0("hello, world from ", myRank, " ", node), all.rank=TRUE)
-else comm.print(paste0("goodbye from ", myRank, " ", node), all.rank=TRUE)
+} else comm.print(paste0("goodbye from ", myRank, " ", node), all.rank=TRUE)
 
 if(comm.rank() == 0) print(date())
 N.gbd <- 1e7
