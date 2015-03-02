748ad7d1dfe5bb6a5b8c43e1951010436082d754
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-03-02 15:44:23 -0800

    added pbd linalg example

---
+++ pbd-linalg.R
@@ -0,0 +1,39 @@
+## @knitr pbd-linalg
+
+library(pbdDMAT, quiet = TRUE )
+init()
+
+n <- 4096*2
+
+library(RhpcBLASctl)
+blas_set_num_threads(1)
+
+init.grid()
+
+if(comm.rank()==0) print(date())
+
+# pbd allows for parallel I/O, but here
+# we keep things simple and distribute
+# an object from one process
+if(comm.rank() == 0) {
+    x <- rnorm(n^2)
+    dim(x) <- c(n, n)
+} else x <- NULL
+dx <- as.ddmatrix(x)
+
+timing <- comm.timer(sigma <- crossprod(dx))
+
+if(comm.rank()==0) {
+    print(date())
+    print(timing)
+}
+
+timing <- comm.timer(out <- chol(sigma))
+
+if(comm.rank()==0) {
+    print(date())
+    print(timing)
+}
+
+
+finalize()
